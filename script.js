783ba61e97aa09424f1876fb1428069f8748915b
Daouda WATTARA <d.wattara@aplusaresearch.com>
2023-05-17 18:11:15 +0200

    search bar update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const arrow_link = document.getElementById('arrow_link');
 const border_bottom = document.querySelector('.border_bottom');
-
+const search_lang = document.querySelector('.search_lang');
+const search_lang_hiden = $('.search_lang li:not(:first-child)');
+const search_input = document.querySelector('.search_input');
+const bt_search = document.querySelector('.bt_search');
 
 
 setInterval(() => {
@@ -25,3 +28,20 @@ border_bottom.style.width = `${randomNumberWidth}rem`
 
 let randomNumberMarginLeft = Math.floor(Math.random() * 5) + -2;
 border_bottom.style.marginLeft = `${randomNumberMarginLeft}rem`
+
+search_lang.addEventListener('mouseover', () => {
+    search_lang_hiden.show();
+
+});
+
+search_lang.addEventListener('mouseout', () => {
+    search_lang_hiden.hide();
+
+});
+
+bt_search.addEventListener('click', () => {
+    search_input.classList.toggle('show');
+    search_lang.classList.toggle('hide');
+    search_input.classList.add('animationSearchButton');
+    search_lang.classList.add('animationSearchButton');
+});
